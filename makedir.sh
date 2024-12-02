8bbf68d12a3f4dd66e68396cf3a0c2f72029f7ef
vasyl.bhd <vasyl.bhd@gmail.com>
2024-12-02 20:21:13 +0200

    make it more automotive

--- makedir.sh
+++ makedir.sh
@@ -1,4 +1,12 @@
-DIRNAME=day$1
+base_folder="."
+
+highest_day=$(ls -d ${base_folder}/day* 2>/dev/null | grep -oP 'day\K[0-9]+' | sort -n | tail -n 1)
+if [ -z "$highest_day" ]; then
+  highest_day=0
+fi
+next_day=$((highest_day + 1))
+
+DIRNAME=day$next_day
 
 mkdir -p $DIRNAME/p1 $DIRNAME/p2
 touch $DIRNAME/input.txt
