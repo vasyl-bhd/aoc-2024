2bf40754230e39cb7662237847c4eb861dbc20e5
vbohdanets <vbohdanets@hubspot.com>
2024-12-06 11:13:41 +0200

    Fix makedir

--- makedir.sh
+++ makedir.sh
@@ -1,13 +1,20 @@
+#!/bin/bash
+
 base_folder="."
 
-highest_day=$(ls -d ${base_folder}/day* 2>/dev/null | grep -oP 'day\K[0-9]+' | sort -n | tail -n 1)
+# Find the highest day folder
+highest_day=$(ls -d ${base_folder}/day* 2>/dev/null | sed -E 's/.*day([0-9]+)/\1/' | sort -n | tail -n 1)
 if [ -z "$highest_day" ]; then
   highest_day=0
 fi
+
+# Calculate the next day number
 next_day=$((highest_day + 1))
 
-DIRNAME=day$next_day
+# Create the new directory and subdirectories
+DIRNAME="day$next_day"
+mkdir -p "$DIRNAME/p1" "$DIRNAME/p2"
 
-mkdir -p $DIRNAME/p1 $DIRNAME/p2
-touch $DIRNAME/input.txt
-echo package main | tee $DIRNAME/p1/p1.go $DIRNAME/p2/p2.go > /dev/null
+# Create an input file and Go files with boilerplate code
+touch "$DIRNAME/input.txt"
+echo "package main" | tee "$DIRNAME/p1/p1.go" "$DIRNAME/p2/p2.go" > /dev/null
